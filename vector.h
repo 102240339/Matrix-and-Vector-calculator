8e99bbd4ebcb6990f591e5f151f799f7290fab46
Acel <102240339@student.vgu.edu.vn>
2026-01-06 22:25:46 +0700

    Update vector.h

--- vector.h
+++ vector.h
@@ -22,7 +22,6 @@ public:
 	void zeros() const;
 	void random() const;
 	void print(string name);
-	double norm(const int& p);
 	//operator
 	double& operator () (const int& s) {
 		assert(s > -1 && s < size); return entry[s];
@@ -57,14 +56,5 @@ public:
 			v += v1(i) * v2(i);
 		}
 		return v;
-	}
-	friend double angle(const Vector& v1, const Vector& v2) {
-		assert(v1.getSize() == v2.getSize());
-		double n1 = 0, n2 = 0; double a = 0;
-		for (int i = 0; i < v1.getSize(); ++i) {
-			n1 += pow(v1(i), 2); n2 += pow(v2(i), 2);
-		}
-		n1 = pow(n1, 1 / 2); n2 = pow(n2, 1 / 2); 
-		return a = acos(dot(v1, v2) / (n1*n2));
 	}
 };
